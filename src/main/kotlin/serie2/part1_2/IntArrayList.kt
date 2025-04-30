48c1602ce8218ee899e5b337613571b8618c1427
SergioOliveira5 <145005002+SergioOliveira5@users.noreply.github.com>
2025-04-30 10:54:33 +0100

    IntArrayList
    praticamente acabado falta só correr o último test

--- src/main/kotlin/serie2/part1_2/IntArrayList.kt
+++ src/main/kotlin/serie2/part1_2/IntArrayList.kt
@@ -1,24 +1,45 @@
 package serie2.part1_2
 
-class IntArrayList : Iterable <Int> {
+class IntArrayList(private val capacity:Int) /* : Iterable <Int> */ {
+    private var size = 0 // tamanho da lista
+    private val values = IntArray(capacity)
+    private var tail = 0 // tail é o índice do último elemento da lista
+    private var head = 0 // head é o índice do primeiro elemento da lista
 
-    fun append(x:Int):Boolean {
-        TODO("Not yet implemented")
+    fun append(x:Int):Boolean { // adiciona elemento x ao fim da lista
+        if(size == capacity) return false // lista cheia
+        values[tail] = x // adiciona o elemento
+        tail += 1 // incrementa o tail
+        size ++ // incrementa o tamanho
+        return true // retorna true
     }
 
     fun get(n:Int):Int?  {
-        TODO("Not yet implemented")
+        return if ( n !in 0 until size) {
+            null // se n não está no intervalo [0, size) retorna null
+        } else {
+            values[n] // retorna o elemento na posição n
+        }
     }
 
     fun addToAll(x:Int)   {
-        TODO("Not yet implemented")
+        for (i in 0 until size) {
+            values[i] += x // adiciona x a todos os elementos
+        }
+
     }
 
     fun remove():Boolean {
-        TODO("Not yet implemented")
+        if (size == 0) return false // lista vazia
+        values[head] = 0 // remove o elemento
+        head += 1 // incrementa o head
+        size --   // decrementa o tamanho
+        return true
     }
-
+/*
     override fun iterator(): Iterator<Int> { // Opcional
-        TODO("Not yet implemented")
+
+
     }
+ */
 }
--- src/test/kotlin/serie2/part4/HashMapTest.kt
+++ src/test/kotlin/serie2/part4/HashMapTest.kt
@@ -1,7 +1,7 @@
 package serie2.part4
 
 import kotlin.test.*
-
+/*
 class HashMapTest {
 
     // Test put one entry and get value
@@ -97,3 +97,5 @@ class HashMapTest {
     }
 
 }
+
+*/
